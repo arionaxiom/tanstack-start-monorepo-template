64477fa714cf452c3911230f044d80f5609dff53
arion <arion.axiom@gmail.com>
2026-05-08 13:37:52 +0700

    feat(test-utils): extract shared TestProviders wrapper
    New workspace package centralizes the Lingui I18nProvider wrapper
    and customRender / customRenderHook helpers. Replaces the duplicated
    test-utils.tsx files in packages/ui and packages/react-hooks. Exports
    fireEvent, render, renderHook, screen, act, userEvent so consumers
    have a single import for everything they need.

---
+++ packages/test-utils/src/react.tsx
@@ -0,0 +1,34 @@
+import { i18n } from "@lingui/core";
+import { I18nProvider } from "@lingui/react";
+import {
+  type RenderHookResult,
+  type RenderOptions,
+  type RenderResult,
+  render,
+  renderHook,
+} from "@testing-library/react";
+import type { ReactElement, ReactNode } from "react";
+
+function TestProviders({ children }: { children: ReactNode }) {
+  return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
+}
+
+function customRender(
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+): RenderResult {
+  return render(ui, { wrapper: TestProviders, ...options });
+}
+
+function customRenderHook<TProps, TResult>(
+  hook: (props: TProps) => TResult,
+  options?: Omit<Parameters<typeof renderHook>[1], "wrapper"> & {
+    initialProps?: TProps;
+  }
+): RenderHookResult<TResult, TProps> {
+  return renderHook(hook, { wrapper: TestProviders, ...options });
+}
+
+export { customRender as render, customRenderHook as renderHook };
+export { fireEvent, screen, act } from "@testing-library/react";
+export { default as userEvent } from "@testing-library/user-event";
